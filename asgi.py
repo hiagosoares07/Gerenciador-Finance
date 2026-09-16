c96bfb995653b697acd0dcf9171e1d9cac5dfd70
hiagosoares07 <hisoares07@gmail.com>
2026-09-16 19:21:04 -0300

    feitas algumas alterações

---
+++ asgi.py
@@ -0,0 +1,12 @@
+import os
+import sys
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+sys.path.insert(0, str(BASE_DIR / 'i9tmg'))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+from django.core.asgi import get_asgi_application
+
+application = get_asgi_application()
